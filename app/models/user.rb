fa7c499c3a0db1ab5ffd2918b431f23b532730b4
KennyUTN <asisignacio@gmail.com>
2021-04-07 18:32:53 -0300

    review fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   validates :username, length: { minimum: 4, maximun: 12 }
   validates :username, :email, presence: true
-  validates :username, :email, uniqueness: true
+  validates :email, :email, uniqueness: true
 end
